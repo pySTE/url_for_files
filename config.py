37db25ee2c74d132d6dce08880ff0f20b7676e25
mar1k <mgazaev291@gmail.com>
2025-07-02 20:30:34 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    UPLOAD_DIR = "uploads"
+    MAX_FILE_SIZE = 1024 * 1024 * 100
+    ALLOW_EXT = {'.jpg', '.jpeg', '.png', '.gif', '.pdf', '.txt', '.mp4', '.mp3', '.zip'}
+    BASE_URL = "http://127.0.0.1:8000"
--- routes/for_url.py
+++ routes/for_url.py
@@ -4,12 +4,14 @@ from pathlib import Path
 import os
 import uuid
 
+from config import Config
+
 router = APIRouter()
 
-UPLOAD_DIR = "uploads"
-MAX_FILE_SIZE = 1024 * 1024 * 100
-ALLOW_EXT = {'.jpg', '.jpeg', '.png', '.gif', '.pdf', '.txt', '.mp4', '.mp3', '.zip'}
-BASE_URL = "http://127.0.0.1:8000"
+UPLOAD_DIR = Config.UPLOAD_DIR
+MAX_FILE_SIZE = Config.MAX_FILE_SIZE
+ALLOW_EXT = Config.ALLOW_EXT
+BASE_URL = Config.BASE_URL
 
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
